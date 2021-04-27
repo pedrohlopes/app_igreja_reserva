1e1ca2d72cd8b4e75747802f41f54b7a7f0b9973
pedrohlopes <pedro.hleite@g.globo>
2021-04-27 10:47:59 -0300

    conflt solved

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import MovieContext from "../contexts/MovieContext"
 import "./styles/form.css"
 type FormData = {
@@ -12,15 +12,13 @@ function randomNumber() {
     return result;
 }
 
-const postConfigFetch = async (data,url) => {
-        console.log("teste" + data)
-		const response = await fetch(url,{method: 'POST', headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'token 00f56408-d336-4f4c-8a50-99f17637905d'
-    }, body: JSON.stringify(data)} );
-		const jsonData = await response.json();
-    
-	};
+const getConfigFetch = async (url) => {
+	const response = await fetch(url);
+	const jsonData = await response.json();
+    return jsonData;
+};
+
+
 
 const format_number = (number) => {
     let out = '';
@@ -36,14 +34,29 @@ const Form = () => {
     const [nome, setName] = useState('');
     const [sobrenome, setName2] = useState('');
     const [submitted, setSubmit] = useState(false);
+    const [formError, setError] = useState(false);
     var d = new Date()
     let nova_reserva = {}
     const {
         formState
     } = useForm();
+
+    const postConfigFetch = async (data, url) => {
+        console.log('Config:',JSON.stringify(data))
         
-    const handleSubmit = (e) => {
+        console.log("teste", data)
+        const response = await fetch(url,{method: 'POST', headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'token 00f56408-d336-4f4c-8a50-99f17637905d'
+        }, body: JSON.stringify(data)} );
+        const jsonData = await response.json();
+        context.changeState(data);
+        console.log(context);
+        };
+
         
+    const handleSubmit = async (e) => {
+        let errorFlag = false;
         e.preventDefault();
         console.log(e)
         console.log(`Form submitted, ${nome} ${sobrenome}`);
@@ -54,21 +67,43 @@ const Form = () => {
                 Data: format_number(d.getDate()) + '/' + format_number(d.getMonth() + 1) + '/' + format_number(d.getFullYear()) + '-' + format_number(d.getHours()) + ':' + format_number(d.getMinutes()) + ':' + format_number(d.getSeconds()),
                 numero: randomNumber()
             };
-            new_object = {
-                ...config,
-                ocupados: [...config.ocupados.concat(config.seatNumbers)],
-                seatNumbers: [],
-                totalSelected: 0,
-                totalOcupados: config.totalOcupados+config.totalSelected,
-                reservas: [...config.reservas, nova_reserva]
-            };
-            context.changeState(new_object);
-            console.log("enviando: " + JSON.stringify(new_object));
-            postConfigFetch(new_object,config.url);
-            console.log(new_object);
-            setName('');
-            setName2('');
-            setSubmit(true);
+
+            let curr_config = await getConfigFetch(config.url);
+            config.seatNumbers.forEach(element => {
+                if (curr_config.ocupados.includes(element)) {
+                    errorFlag = true;
+                }
+            });
+            
+            if (errorFlag) {
+                const new_object = await {
+                    ...curr_config,
+                };
+                context.changeState(new_object)
+                setError(true);
+                setSubmit(false);
+            }
+            else {
+                const new_object = await {
+                    ...curr_config,
+                    ocupados: [...curr_config.ocupados.concat(config.seatNumbers)],
+                    seatNumbers: [],
+                    totalSelected: 0,
+                    totalOcupados: curr_config.totalOcupados+config.totalSelected,
+                    reservas: [...curr_config.reservas, nova_reserva]
+                };
+
+
+                postConfigFetch(new_object, config.url);
+                console.log(new_object);
+                setName('');
+                setName2('');
+                setError(false);
+                setSubmit(true);
+                
+                
+            }
+            
         }
         
 
@@ -85,8 +120,14 @@ const Form = () => {
                 </li>
                 
                 {submitted && (
-                <div className="success">
-                    <li>Reserva realizada com sucesso! Bom culto!</li>
+                    <div className="success">
+                        <li>Reserva realizada com sucesso! Bom culto!</li>
+                    </div>
+                )}
+                
+                {formError && (
+                <div className="fail">
+                    <li>Houve um problema com sua reserva. Tente novamente.</li>
                 </div>
                 )}
             </ul>
